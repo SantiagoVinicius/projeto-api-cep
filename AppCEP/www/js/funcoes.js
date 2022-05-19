66bd0d1515cb630bc4f4f8724fb098acbd48fd5c
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2022-05-18 21:44:21 -0300

    AppCEP

--- AppCEP/www/js/funcoes.js
+++ AppCEP/www/js/funcoes.js
@@ -1,5 +1,17 @@
 function BuscarCEP(){
-    console.log("Essa é a função de pesquisa de CEP")
+   let cep =  document.getElementById("cep").value
+   $.ajax({
+       type: "GET",
+       url: `https://viacep.com.br/ws/${cep}/json`,
+       success: (function(endereco){
+           let bairro =  endereco.bairro
+           let rua = endereco.logradouro
+           let cidade = endereco.localidade
+           let uf = endereco.uf
+           document.getElementById("endereco-completo").innerHTML = `Rua/Avenida: ${rua} <br> Bairro: ${bairro} <br> Cidade: ${cidade} - ${uf}`
+
+       })
+   })
 }
 
 document.getElementById("pesquisar").addEventListener("click",function(){BuscarCEP()})
